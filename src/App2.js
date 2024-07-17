475aeffe18f874b592c8b85609b550f470e287f4
4thdraw <4thdraw2@gmail.com>
2024-07-17 09:45:51 +0900

    최적화

--- src/App2.js
+++ src/App2.js
@@ -2,14 +2,7 @@ import React from 'react';
 import './App.css';
 import list from './json/list.json'
 
-
-
-function hovereffect (num){
-    document.querySelectorAll(".list li").item(num).classList.add('red');
-}
-const leaveeffect = (num) =>{
-  document.querySelectorAll(".list li").item(num).classList.remove('red');
-}
+import { hovereffect, leaveeffect } from './common/module/hovereffect'
 
 function App() {
  
@@ -19,7 +12,7 @@ function App() {
       {
         list.notice.map((listvalue, idx)=>{
           return(
-            <li onMouseEnter={()=>{ hovereffect (idx)}}
+            <li onMouseEnter={()=>{ hovereffect(idx)}}
                 onMouseLeave={()=>{ leaveeffect(idx)}}
             >{listvalue}</li>
           )
